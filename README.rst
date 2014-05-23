c9b4680c675053616cdfa9c76850a92dc7db5eb6
Tai Lee <real.human@mrmachine.net>
2014-05-23 12:42:10 +1000

    Initial commit, taken from Django 1.5.8.

---
+++ README.rst
@@ -0,0 +1,2 @@
+This is ``django.contrib.markup`` renamed to ``django_markup``, taken directly
+from Django 1.5.8.
---
+++ setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    name='django-markup',
+    version='1.5.8',
+    packages=setuptools.find_packages()
+)
